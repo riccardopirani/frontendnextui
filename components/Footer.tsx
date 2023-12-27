52455d0c2abb8d9cbe0795e43f578079a8a9bb03
riccardop <riccardopirani@users.noreply.github.com>
2023-12-27 23:19:53 +0100

    Footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -58,7 +58,7 @@ const Footer = () => {
 
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">
-          2023 Hilink | All rights reserved
+          2023 Marconi Software | All rights reserved
         </p>
       </div>
     </footer>
